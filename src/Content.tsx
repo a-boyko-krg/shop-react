90bba30dff10c8e7f995ec43e0610c6c2c8d4dfa
Andrii <a.boyko.krg@gmail.com>
2022-12-11 10:56:00 +0200

    css

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,3 +1,5 @@
+import './Content.css'
+
 type Props = {
     p1: string
     p2: string
@@ -5,7 +7,7 @@ type Props = {
 const Content = ({ p1, p2 }: Props) => {
     return (
         <>
-            <p>{p1}</p>
+            <p className="tomato">{p1}</p>
             <p>{p2}</p>
         </>
     )
