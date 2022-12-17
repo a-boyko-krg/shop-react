0ca9f6374916b96ea868bfb13d2a110d2d897b5f
Andrii <a.boyko.krg@gmail.com>
2022-12-17 09:15:05 +0200

    end lesson3

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Content from './Content'
 
-type TitleProps = {
-    title: string
-    text?: string
-}
 
-const Title = ({ title, text = 'TEST' }: TitleProps) => {
-    return (
-        <h1>
-            {text} {title}
-        </h1>
-    )
-}
 const App = () => {
     return (
         <>
-            <Title text="Hello" title="React" />
-            <Content
-            p1=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-                placeat maxime eligendi, assumenda asperiores ducimus delectus
-                blanditiis sapiente debitis! Distinctio adipisci nobis illum
-                accusamus est exercitationem minus voluptatibus beatae
-                molestias."
-            p2=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-                placeat maxime eligendi, assumenda asperiores ducimus delectus
-                blanditiis sapiente debitis! Distinctio adipisci nobis illum
-                accusamus est exercitationem minus voluptatibus beatae
-                molestias."
-                />      
         </>
     )
 }
