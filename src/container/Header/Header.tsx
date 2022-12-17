873cd5af0bde4190fe3d225b671abb5ed4ef3585
Andrii <a.boyko.krg@gmail.com>
2022-12-17 19:07:52 +0200

    add Logo

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,16 +1,16 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/material/Container'
 import './Header.scss'
 import Menu from 'components/Menu/Menu'
+import logo from 'assets/logo.svg'
 
 type Props = {}
 const Header = (props: Props) => {
     return (
-        <AppBar position="static" className='app-bar'>
+        <AppBar position="static" className="app-bar">
             <Container maxWidth="lg">
                 <Toolbar>
                     <IconButton
@@ -22,14 +22,10 @@ const Header = (props: Props) => {
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        News
-                    </Typography>
-                    <Menu/>
+                    <div className="logo">
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <Menu />
                 </Toolbar>
             </Container>
         </AppBar>
