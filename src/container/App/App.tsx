de8f587971705d871aa0e81e405c07567131a27b
Andrii <a.boyko.krg@gmail.com>
2022-12-17 17:41:10 +0200

    install mui and CssBaseLine

---
+++ src/container/App/App.tsx
@@ -0,0 +1,14 @@
+import Header from 'container/Header/Header'
+import CssBaseline from '@mui/material/CssBaseline'
+
+type Props = {}
+
+const App = (props: Props) => {
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+        </>
+    )
+}
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-
-const App = () => {
-    return (
-        <>
-        </>
-    )
-}
+import App from 'container/App/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
