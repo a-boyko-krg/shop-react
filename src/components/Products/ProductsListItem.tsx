3f027c069bd61a24d5aa4230ae0b9261227eb4f8
Andrii <a.boyko.krg@gmail.com>
2022-12-25 00:21:07 +0200

    add State

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -17,27 +17,63 @@ type Props = {
     image: string
 }
 
-class ProductsListItem extends Component <Props> {
+type State = {
+    count: number
+}
+
+class ProductsListItem extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            count: 1,
+        }
+        this.onIncrementClick = this.onIncrementClick.bind(this)
+        this.onDecrementClick = this.onDecrementClick.bind(this)
+    }
+
+    onIncrementClick() {
+        this.setState((prevState: State) => ({
+            count: prevState.count + 1,
+        }))
+    }
+
+    onDecrementClick() {
+        this.setState((prevState: State) => ({
+            count: prevState.count - 1,
+        }))
+    }
+
     render() {
+        const { title, desc, type, capacity, price, image } = this.props
         return (
             <Card className="product-list-item">
                 <CardContent>
                     <div className="product-img">
-                        <img src={this.props.image} alt="iphone" />
+                        <img src={image} alt="iphone" />
                     </div>
-                    <h4 className="product-title">{this.props.title}</h4>
-                    <p className="product-desc">{this.props.desc}</p>
+                    <h4 className="product-title">{title}</h4>
+                    <p className="product-desc">{desc}</p>
                     <div className="product-features">
-                        <span>Type:</span> {this.props.type}
+                        <span>Type:</span> {type}
                     </div>
                     <div className="product-features">
-                        <span>Capacity:</span> {this.props.capacity} gb
+                        <span>Capacity:</span> {capacity} gb
                     </div>
-                    <div className="product-price">{this.props.price}₴ </div>
+                    <div className="product-price">{price}₴ </div>
                     <div className="product-quantity">
-                        <Button variant="outlined">-</Button>
-                        <TextField size="small" value="1" />
-                        <Button variant="outlined">+</Button>
+                        <Button
+                            variant="outlined"
+                            onClick={this.onDecrementClick}
+                        >
+                            -
+                        </Button>
+                        <TextField size="small" value={this.state.count} />
+                        <Button
+                            variant="outlined"
+                            onClick={this.onIncrementClick}
+                        >
+                            +
+                        </Button>
                     </div>
                 </CardContent>
 
