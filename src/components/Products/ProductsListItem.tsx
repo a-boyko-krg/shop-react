b89a5c03313acc5e51360053e95758f6cd326352
Andrii <a.boyko.krg@gmail.com>
2022-12-26 01:57:40 +0200

    add count hooks

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -5,6 +5,7 @@ import {
     Button,
     TextField,
 } from '@mui/material'
+import { useState } from 'react'
 import './ProductsListItem.scss'
 
 type Props = {
@@ -24,6 +25,15 @@ const ProductsListItem = ({
     price,
     image,
 }: Props) => {
+    const [count, setCount] = useState<number>(1)
+
+    const onIncrement = () => {
+        setCount((prevState: number) => prevState + 1)
+    }
+    const onDecrement = () => {
+        setCount((prevState: number) => prevState - 1)
+    }
+
     return (
         <Card className="product-list-item">
             <CardContent>
@@ -40,9 +50,13 @@ const ProductsListItem = ({
                 </div>
                 <div className="product-price">{price}₴ </div>
                 <div className="product-quantity">
-                    <Button variant="outlined">-</Button>
-                    <TextField size="small" value="1" />
-                    <Button variant="outlined">+</Button>
+                    <Button variant="outlined" onClick={() => onDecrement()}>
+                        -
+                    </Button>
+                    <TextField size="small" value={count} />
+                    <Button variant="outlined" onClick={() => onIncrement()}>
+                        +
+                    </Button>
                 </div>
             </CardContent>
 
