e967217fe68db2bddd15849064ed8ebf2dd9dae2
Andrii <a.boyko.krg@gmail.com>
2022-12-27 13:20:28 +0200

    add function addProductToCart

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -11,15 +11,23 @@ type CartDataProps = {
 
 const App = () => {
     const [cartData, setCartData] = useState<CartDataProps>({
-        totalCount: 10,
-        totalPrice: 100,
+        totalCount: 0,
+        totalPrice: 0,
     })
+
+    const addProductToCart = (total:number, price:number) => {
+        setCartData((prevState:CartDataProps) => ({
+            totalCount: prevState.totalCount + total,
+            totalPrice: prevState.totalPrice + total * price,
+        }))
+    }
 
     return (
         <>
             <StyledEngineProvider injectFirst>
                 <CssBaseline />
                 <Header cartData = {cartData}/>
+                <button onClick={() => addProductToCart(5,500)}>add to cart</button>
                 <Main/>
             </StyledEngineProvider>
         </>
