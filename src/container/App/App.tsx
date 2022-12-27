13539dcbbf58b7827b23b11e2b665be34ff87fbd
Andrii <a.boyko.krg@gmail.com>
2022-12-27 13:03:15 +0200

    add count and price

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -2,15 +2,24 @@ import Header from 'container/Header/Header'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
 import Main from 'container/Main/Main'
+import {useState} from 'react'
 
-type Props = {}
+type CartDataProps = {
+    totalCount: number
+    totalPrice: number
+}
 
-const App = (props: Props) => {
+const App = () => {
+    const [cartData, setCartData] = useState<CartDataProps>({
+        totalCount: 10,
+        totalPrice: 100,
+    })
+
     return (
         <>
             <StyledEngineProvider injectFirst>
                 <CssBaseline />
-                <Header />
+                <Header cartData = {cartData}/>
                 <Main/>
             </StyledEngineProvider>
         </>
--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -8,8 +8,13 @@ import Menu from 'components/Menu/Menu'
 import logo from 'assets/logo.svg'
 import CartHeader from 'components/CartHeader/CartHeader'
 
-type Props = {}
-const Header = (props: Props) => {
+type Props = {
+    cartData: { 
+        totalCount: number 
+        totalPrice: number 
+    }
+}
+const Header = ({cartData}: Props) => {
     return (
         <AppBar position="static" className="app-bar">
             <Container maxWidth="lg">
@@ -27,7 +32,7 @@ const Header = (props: Props) => {
                         <img src={logo} alt="logo" />
                     </div>
                     <Menu />
-                    <CartHeader/>
+                    <CartHeader cartData={cartData}/>
                 </Toolbar>
             </Container>
         </AppBar>
