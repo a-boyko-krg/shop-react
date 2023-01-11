63be5bf72adceadc431dc71df7fc2eff64897833
Andrii <a.boyko.krg@gmail.com>
2023-01-11 16:30:56 +0200

    add menu item

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,20 +1,28 @@
 import { Container } from '@mui/material'
+import CartPage from 'pages/CartPage/CartPage'
 import Home from 'pages/Home/Home'
+import { Routes, Route } from 'react-router-dom'
 
 type Props = {
     addProductToCart: (id: number, count: number) => void
 }
-const Main = ({addProductToCart}: Props) => {
-  return (
-      <main
-          style={{
-              padding: '40px 0',
-          }}
-      >
-          <Container maxWidth="lg">
-              <Home addProductToCart={addProductToCart} />
-          </Container>
-      </main>
-  )
+const Main = ({ addProductToCart }: Props) => {
+    return (
+        <main
+            style={{
+                padding: '40px 0',
+            }}
+        >
+            <Container maxWidth="lg">
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home addProductToCart={addProductToCart} />}
+                    />
+                    <Route path="cart" element={<CartPage />} />
+                </Routes>
+            </Container>
+        </main>
+    )
 }
 export default Main
